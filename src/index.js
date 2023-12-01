eb44dbd21cc3893752d75797308d4d268ba43c83
Fishy <109588999+FiZzontje@users.noreply.github.com>
2023-12-01 22:08:26 +0100

    Added status

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-require('dotenv').config();// Use this to protect youre key 
-const { Client, IntentsBitField, Presence } = require('discord.js');
+require('dotenv').config();// Use this to protect you're key 
+const { Client, IntentsBitField, Presence, ActivityType } = require('discord.js');
 
 const client = new Client({
     intents: [
@@ -10,16 +10,22 @@ const client = new Client({
     ],
 });// you can add mode is you want!
 
-client.on('ready', (c) => { //Logs in the console if youre bot is online or not // if this fail you will get errors! 
+client.on('ready', (c) => { //Logs in the console if you're bot is online or not // if this fail you will get errors! 
     console.log(`✅ ${c.user.tag} is online`);
+
+    client.user.setActivity({
+        name: "Dev mode by fisy",
+        type: ActivityType.Watching,
+
+    })
 
 });
 
-//  client.on('messageCreate', (message) => { //Post a log in the console when someone sends a message in channels. You can block this code, it is intended for development work
-//      console.log(message);
-//  }); // if you want to use this code for logging in vs console log remove //<---- THis lines! 
+ client.on('messageCreate', (message) => { //Post a log in the console when someone sends a message in channels. You can block this code, it is intended for development work
+      console.log(message);
+}); // if you want to use this code for logging in vs console log remove //<---- THis lines! 
 
-/* client.on('messageCreate', (message) => { //Reply to message and reply's to 'Hi' 
+ client.on('messageCreate', (message) => { //Reply to message and reply's to 'Hi' 
     if (message.author.bot) {
         return;
     }
