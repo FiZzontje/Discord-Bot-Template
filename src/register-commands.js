edcf8b6bb432127a023e3dd3d716db673bcdc802
Fishy <109588999+FiZzontje@users.noreply.github.com>
2023-12-04 02:51:44 +0100

    Added command registers

---
+++ src/register-commands.js
@@ -0,0 +1,33 @@
+require('dotenv').config();
+const { REST, Routes } = require('discord.js');
+
+const commands = [
+    {
+        name: 'hey',
+        description: 'Replies with hey!',
+    },
+    {
+        name: 'ping',
+        description: 'Replies with Pong if the bot is online!',
+    },
+];
+
+const rest = new REST({ version: '10'}).setToken(process.env.TOKEN);
+
+(async () => {
+    try {
+        console.log('registering slash commands..');
+
+
+        await rest.put(
+            Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
+            { body: commands }
+
+        );
+
+        console.log('slash commands successfull..');
+    } catch (error) {
+        console.log(`error: ${error}`);
+    }
+
+})();
